4fb1f5164e0b3418e7b26956d57a0dcc01c338fe
StomperN7 <imnotcrabsnub1@gmail.com>
2022-10-14 23:06:54 +0600

    Add main.cpp file

---
+++ src/main.cpp
@@ -0,0 +1,25 @@
+﻿// pwgen.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+#include "utils.h"
+#include <string>
+#include <iostream>
+#include <unistd.h>
+
+
+int main()
+{
+    std::setlocale(LC_ALL, "en_US.UTF-8");
+    std::string alphabetz = "";
+    int n = 0;
+    std::string key = "";
+
+    std::cout << "Available key options: p n y N" << std::endl;
+    std::cout << "Enter key: -";
+    std::cin >> key;
+    alphabetz = utils::setupPassAlphabet(key);
+
+    std::cout << "Enter pass length: ";
+    std::cin >> n;
+    std::cout << utils::randomPass(n, alphabetz);
+    
+}
