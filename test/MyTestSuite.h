6ec029f3410131668229cd1f6edd02e0e6d063a4
StomperN7 <imnotcrabsnub1@gmail.com>
2022-11-05 16:39:11 +0600

    Rework unix tests

--- test/MyTestSuite.h
+++ test/MyTestSuite.h
@@ -4,30 +4,48 @@
 class MyTestSuite : public CxxTest::TestSuite
 {
 public:
-   void testRandom( void )
-   {
-	TS_ASSERT_EQUALS( utils::random(2,0), 1 );
-	TS_ASSERT_EQUALS( utils::random(0,0), 1 );
-	TS_ASSERT_DIFFERS( utils::random(2,3), 1 );
-	TS_ASSERT_DIFFERS( utils::random(4,10), 1 );
-	TS_ASSERT_DIFFERS( utils::random(10,100), 1 );
-	TS_ASSERT_DIFFERS( utils::random(0,999), 1 );
-   }
-   void testSetupAlphabet( void )
+   void testSetupAlphabet1( void )
    {
 	TS_ASSERT_DIFFERS( utils::setupPassAlphabet("n"), "1" );
+   }
+   void testSetupAlphabet2( void )
+   {
 	TS_ASSERT_DIFFERS( utils::setupPassAlphabet("p"), "1" );
+   }
+   void testSetupAlphabet3( void )
+   {
 	TS_ASSERT_DIFFERS( utils::setupPassAlphabet("y"), "1" );
+   }
+   void testSetupAlphabet4( void )
+   {
 	TS_ASSERT_DIFFERS( utils::setupPassAlphabet("N"), "1" );
+   }
+   void testSetupAlphabet5( void )
+   {
 	TS_ASSERT_DIFFERS( utils::setupPassAlphabet("npy"), "1" );
+   }
+   void testSetupAlphabet6( void )
+   {
 	TS_ASSERT_EQUALS( utils::setupPassAlphabet("z"), "1" );
    }
-   void testPassGen( void )
+   void testPassGen1( void )
    {
-	TS_ASSERT_DIFFERS( utils::randomPass(1,"abcdefghijklmnopqrstuvwxyz"), "1");
-	TS_ASSERT_DIFFERS( utils::randomPass(10,"abcdefghijklmnopqrstuvwxyz"), "1");
-	TS_ASSERT_DIFFERS( utils::randomPass(100,"abcdefghijklmnopqrstuvwxyz"), "1");
-	TS_ASSERT_DIFFERS( utils::randomPass(999,"abcdefghijklmnopqrstuvwxyz"), "1");
-	TS_ASSERT_EQUALS( utils::randomPass(0,"abcdefghijklmnopqrstuvwxyz"), "1");
+   	TS_ASSERT_DIFFERS( utils::randomPass(1,"abcdefghijklmnopqrstuvwxyz"),"1");
+   }
+   void testPassGen2( void )
+   {
+	TS_ASSERT_DIFFERS( utils::randomPass(10,"abcdefghijklmnopqrstuvwxyz"),"1");
+   }
+   void testPassGen3( void )
+   {
+	TS_ASSERT_DIFFERS( utils::randomPass(100,"abcdefghijklmnopqrstuvwxyz"),"1");
+   }
+   void testPassGen4( void )
+   {
+	TS_ASSERT_DIFFERS( utils::randomPass(999,"abcdefghijklmnopqrstuvwxyz"),"1");
+   }
+   void testPassGen5( void )
+   {
+	TS_ASSERT_EQUALS( utils::randomPass(0,"abcdefghijklmnopqrstuvwxyz"),"1");
    }
 };
