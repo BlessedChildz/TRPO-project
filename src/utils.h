e555be37713bad227cc7893b17b94d11b9986e01
StomperN7 <imnotcrabsnub1@gmail.com>
2022-11-05 15:54:45 +0600

    Remove unused headers

--- src/utils.h
+++ src/utils.h
@@ -7,7 +7,4 @@ namespace utils {
 	int random(int min, int max);
 	std::string randomPass(uint32_t length, std::string addToChar);
 	std::string setupPassAlphabet(std::string keyString);
-
-	int testSetupPassAlphabet(std::string keyString, int expected);
-	int testRandomPass(uint32_t length, int expected);
 }
