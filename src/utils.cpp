a211593f6225e0ac4731564485d7034d85490fe9
StomperN7 <imnotcrabsnub1@gmail.com>
2022-11-05 16:26:29 +0600

    Fix alphabet generation

--- src/utils.cpp
+++ src/utils.cpp
@@ -20,7 +20,7 @@ std::string utils::randomPass(uint32_t length, std::string inputAlphabet) {
 }
 
 std::string utils::setupPassAlphabet(std::string keyString) {
-    std::string genAlphabet = "";
+    std::string genAlphabet = "abcdefghijklmnopqrstuvwxyz";
     if (keyString.find("d") != std::string::npos || keyString.find("p") != std::string::npos || keyString.find("n") != std::string::npos || keyString.find("y") != std::string::npos || keyString.find("N") != std::string::npos) {
     	if (keyString.find("d") != std::string::npos) {
             genAlphabet = "abcdefghijklmnopqrstuvwxyz";
@@ -30,7 +30,6 @@ std::string utils::setupPassAlphabet(std::string keyString) {
             genAlphabet = "1234567890";
             return genAlphabet;
         }
-        genAlphabet += "abcdefghijklmnopqrstuvwxyz";
         if (keyString.find("n") != std::string::npos)
             genAlphabet += "1234567890";
         if (keyString.find("p") != std::string::npos)
